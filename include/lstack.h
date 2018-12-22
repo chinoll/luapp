d8a484ad32587f351b56d0ce3f37c6fa79f9d600
chinoll <chinoll@protonmail.com>
2018-12-22 16:09:51 +0800

    Add Lua stack and Lua stack operation functions

---
+++ include/lstack.h
@@ -0,0 +1,26 @@
+
+#ifndef LUAPP_LSTACK_H
+#define LUAPP_LSTACK_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "lvalue.h"
+
+typedef  struct __lua_stack {
+    LuaValue *slots;
+    uint64_t stack_len;
+    uint64_t top;           //stack top index
+} LuaStack;
+LuaValue * newLuaValue(int type,void * data);
+void freeLuaValue(LuaValue * val);
+LuaStack * newLuaStack(uint64_t size);
+void freeLuaStack(LuaStack * stack);
+void check(LuaStack *stack,uint64_t n);
+void push(LuaStack *stack,LuaValue val);
+LuaValue * pop(LuaStack * stack);
+uint64_t absIndex(LuaStack *stack,int64_t idx);
+bool isValid(LuaStack *stack,int64_t idx);
+LuaValue * get(LuaStack *stack,int64_t idx);
+void set(LuaStack *stack,int64_t idx,LuaValue *val);
+void reverse(LuaStack * stack,int64_t from,int64_t to);
+#endif //LUAPP_LSTACK_H
